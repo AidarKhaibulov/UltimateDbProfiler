141ce0ee8dd35ae48d8ba9add5a388683e267acd
Aidar Khaibulov <myakkaynt228@gmail.com>
2023-11-05 17:09:23 +0300

    c1

--- database-profiler/src/main/java/ru/dbp/databaseprofiler/DatabaseProfilerApplication.java
+++ database-profiler/src/main/java/ru/dbp/databaseprofiler/DatabaseProfilerApplication.java
@@ -4,11 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 @SpringBootApplication
 @EnableScheduling
 public class DatabaseProfilerApplication {
